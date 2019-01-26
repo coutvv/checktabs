96dafe40709e0d3fae742a6eac2e9e8fd1155eaa
Lomovtsev Roman <coutvv@gmail.com>
2019-01-26 20:28:16 +0300

    init

---
+++ src/main/java/me/lomovtsev/chtabs/EntryPoint.java
@@ -0,0 +1,11 @@
+package me.lomovtsev.chtabs;
+
+/**
+ * @author coutvv    26/01/2019
+ */
+public class EntryPoint {
+
+    public static void main(String[] args) {
+        System.out.println("Check tabs in the super puper github account");
+    }
+}
