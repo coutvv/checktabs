30a95fbb476b5efaf2aca9d8b601beb2ca3c4162
Lomovtsev Roman <coutvv@gmail.com>
2019-01-26 20:42:09 +0300

    PR-0001 meh

--- src/main/java/me/lomovtsev/chtabs/EntryPoint.java
+++ src/main/java/me/lomovtsev/chtabs/EntryPoint.java
@@ -7,5 +7,7 @@ public class EntryPoint {
 
     public static void main(String[] args) {
         System.out.println("Check tabs in the super puper github account");
+
+		System.out.println("meh");
     }
 }
